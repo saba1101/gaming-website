a75faa01660e9570ef92f0439fd83135c5e39cb5
saba2446 <sabakhara44@gmail.com>
2023-11-12 01:26:40 +0400

    ADD: full vite config, scss, tokens,mixins

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  server: {
+    port: 5005,
+    open: true, 
+  },
+
+  build: {
+    outDir: 'dist',
+    minify: 'terser',
+    sourcemap: true,
+  },
+
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
+
+  css: {
+    modules: true,
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+            @import "@/styles/_variables.scss";
+            @import "@/styles/_mixins.scss";
+            @import "@/styles/_tokens.scss";
+        `,
+      },
+    },
+  },
+});

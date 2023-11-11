faf9e4ed0c6c3842b72db1f2ed79212a2a1d1284
saba2446 <sabakhara44@gmail.com>
2023-11-12 01:37:45 +0400

    ADD: React Router ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-function App() {
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Home from '@/pages/home/Home';
+const App = () => {
 
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </Router>
   )
 }
 

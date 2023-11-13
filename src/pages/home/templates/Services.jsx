a3476d979ef550a86121b070244af498aa774257
saba2446 <sabakhara44@gmail.com>
2023-11-13 23:45:16 +0400

    ADD: Services

--- src/pages/home/templates/Services.jsx
+++ src/pages/home/templates/Services.jsx
@@ -1,5 +1,12 @@
 import style from '@/pages/home/Home.module.scss'
+import { CONTENT_MANAGER } from '../../../utils/Data'
+import { useEffect, useState } from 'react'
 const Services = () => {
+    const [Services, setServices] = useState([])
+
+    useEffect(() => {
+        setServices(CONTENT_MANAGER.SERVICES)
+    }, [])
     return (
         <>
             <div className={style.templateServices}>
@@ -7,7 +14,20 @@ const Services = () => {
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
                 <div className={style.servicesList}>
                     <ul>
-
+                        {
+                            Services && Services.map((service, index) => {
+                                return (
+                                    <>
+                                        <li key={index} data-aos="fade-up" data-aos-delay={`${index}00`}>
+                                            <div className={style.iconWrapper}>
+                                                {<service.Image />}
+                                            </div>
+                                            <span>{service.Label}</span>
+                                        </li>
+                                    </>
+                                )
+                            })
+                        }
                     </ul>
                 </div>
             </div>

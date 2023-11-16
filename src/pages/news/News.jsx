2e68c5c7909be0cadee239ff09e47451b3f9d373
saba2446 <sabakhara44@gmail.com>
2023-11-16 17:21:07 +0400

    ADD: News,Data,Template.

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,5 +1,92 @@
-const News = () => {
+import { useState } from 'react'
+import Search from '../../components/search/Search'
+import style from '@/pages/news/News.module.scss'
+import { CONTENT_MANAGER } from '../../utils/Data'
+import EmptyArticleImage from '@/assets/images/news.jpg'
 
+const News = () => {
+    const [SearchValue, setSearchValue] = useState('')
+    const [News, setNews] = useState([])
+
+    useState(() => {
+        setNews(CONTENT_MANAGER.NEWS)
+    }, [])
+    return (
+        <>
+            <section className={style.sectionNewsIntro}>
+                <h1>Lorem Ipsum is simply dummy text of the printing and.</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                <Search
+                    value={SearchValue}
+                    onChange={(value) => setSearchValue(value)}
+                />
+                <div
+                    className={style.newsContent}
+                    data-aos="fade-right"
+                >
+                    <div className={style.mainArticles}>
+                        <ul>
+                            {News &&
+                                News.slice(0, 3).map((article, index) => {
+                                    return (
+                                        <li
+                                            key={index}
+                                            data-aos={'fade-bottom'}
+                                        >
+                                            <div className={style.articleImage}>
+                                                <img src={article.Image} />
+                                            </div>
+                                            <div className={style.articleDetails}>
+                                                <div className={style.authorDetails}>
+                                                    <span>
+                                                        {' '}
+                                                        <strong>Author</strong>: {article.Author}
+                                                    </span>
+                                                    <span>{article.Date}</span>
+                                                </div>
+                                                <h1>{article.Label}</h1>
+                                                <p>{article.Message}</p>
+                                                <div className={style.readMore}>
+                                                    <span>Read More</span>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    )
+                                })}
+                        </ul>
+                    </div>
+                    <div
+                        className={style.sideArticles}
+                        data-aos="fade-left"
+                    >
+                        <ul>
+                            {News &&
+                                News.map((article, index) => {
+                                    return (
+                                        <li
+                                            key={index}
+                                            data-aos={'flip-up'}
+                                        >
+                                            <div className={style.imageWrapper}>
+                                                <img src={EmptyArticleImage} />
+                                            </div>
+                                            <div className={style.detailsWrapper}>
+                                                <div className={style.authorDetails}>
+                                                    <h1>{article.Author}</h1>
+                                                </div>
+                                                <div className={style.articleContent}>
+                                                    <p>{article.Message.slice(0, 120)}...</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    )
+                                })}
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default News
--- src/utils/Data.js
+++ src/utils/Data.js
@@ -166,4 +166,72 @@ export const CONTENT_MANAGER = {
             Stars: 4,
         },
     ],
+    NEWS: [
+        {
+            Author: 'PlayerOne87',
+            Message:
+                "In the realm of gaming, exploration isn't just a mechanic; it's a philosophy. It's about diving into the unknown, uncovering hidden gems, and unraveling mysteries that await around every corner. From traversing the vast landscapes of open worlds to delving into the intricacies of narrative-driven experiences, exploration fuels our gaming adventures. The thrill of stumbling upon a breathtaking vista or stumbling into an unexpected encounter keeps us hooked. It's not just about uncovering content but also about self-discovery. Each quest, each hidden treasure, is a step towards mastering the art of exploration in gaming.",
+            Label: 'Exploration',
+            Date: '2023-11-15',
+            Image: Tgame3,
+        },
+        {
+            Author: 'GameMaster99',
+            Message:
+                "Mastering the latest FPS (First-Person Shooter) games requires more than quick reflexes. It's a blend of strategy, precision, and adaptability. Understanding maps, weapon dynamics, and enemy behaviors are the fundamentals. Yet, true mastery goes beyond these basics. It involves analyzing your gameplay, learning from defeats, and fine-tuning your tactics. From mastering recoil patterns to predicting opponent movements, every match is a learning opportunity. Success in FPS games demands not just skill, but also a strategic mindset coupled with a willingness to constantly evolve and adapt to new challenges.",
+            Label: 'Strategy',
+            Date: '2023-11-14',
+            Image: Tgame2,
+        },
+        {
+            Author: 'GamerGirl22',
+            Message:
+                "Embarking on a journey through the fantasy realm within a game is akin to stepping into a portal to another dimension. It's an escape from reality into a world filled with mythical creatures, enchanting landscapes, and epic quests. The allure of forging alliances with magical beings, wielding powerful artifacts, and shaping the fate of realms is irresistible. The heart of fantasy games lies not only in the fantastical elements but also in the emotional connections forged with characters and the immersive storytelling that transports us beyond our imagination.",
+            Label: 'Review',
+            Date: '2023-11-13',
+            Image: Tgame4,
+        },
+        {
+            Author: 'EpicGamer123',
+            Message:
+                "Engaging in multiplayer battles, where strategies clash and skills are put to the test, brings out the competitive spirit in every gamer. It's about teamwork, coordination, and seizing fleeting moments of victory. The rush of adrenaline as you lead your team to triumph, the camaraderie built through shared victories and defeats, defines the essence of multiplayer gaming. Whether it's capturing flags, achieving objectives, or outmaneuvering opponents, multiplayer games provide an endless stream of thrilling experiences.",
+            Label: 'Multiplayer',
+            Date: '2023-11-12',
+        },
+        {
+            Author: 'RetroGamer1980',
+            Message:
+                "Exploring the nostalgic realms of retro gaming is like rediscovering lost treasures from the past. It's about revisiting classic titles, reminiscing about pixelated heroes, and cherishing simpler yet equally captivating gameplay. The charm of retro games lies in their timeless appeal. They evoke memories, offering a glimpse into the evolution of gaming while retaining their unique magic. The simplicity of gameplay mechanics and the challenge they pose remind us of the joy in mastering the basics of gaming.",
+            Label: 'Retro Gaming',
+            Date: '2023-11-11',
+        },
+        {
+            Author: 'Speedster99',
+            Message:
+                "Racing games aren't just about speed; they encapsulate the thrill of competition, precision driving, and the pursuit of perfection. Whether it's maneuvering through hairpin turns, overtaking rivals, or fine-tuning your vehicle, every race is an adrenaline-fueled challenge. The blend of strategy, reflexes, and the quest for that perfect lap time drives the passion for racing games. From realistic simulations to arcade-style fun, the racing genre caters to all, fostering a love for speed and competition.",
+            Label: 'Racing',
+            Date: '2023-11-10',
+        },
+        {
+            Author: 'QuestSeeker76',
+            Message:
+                "Quests in RPGs (Role-Playing Games) offer more than just in-game rewards; they weave tales of heroism, personal growth, and moral dilemmas. It's about immersing oneself in a character's journey, making choices that impact the game world, and forging one's identity within a fantastical realm. The depth of storytelling, character development, and the richness of worlds in RPGs create an immersive experience that extends far beyond the game screen.",
+            Label: 'RPG',
+            Date: '2023-11-09',
+        },
+        {
+            Author: 'ConsoleGuru55',
+            Message:
+                'The evolution of gaming consoles has been a journey marked by technological leaps and revolutionary advancements. From pixelated screens to high-definition graphics, from wired controllers to motion-sensitive interfaces, consoles have continually redefined the gaming experience. Each generation brings forth innovations, pushing the boundaries of what gaming can achieve. The excitement of unboxing a new console and exploring its capabilities resonates with every gaming enthusiast.',
+            Label: 'Console Evolution',
+            Date: '2023-11-08',
+        },
+        {
+            Author: 'MobileGamerPro',
+            Message:
+                'Mobile gaming has transcended traditional boundaries, offering an accessible platform for gaming on the go. With powerful devices in our pockets, we immerse ourselves in diverse gaming experiences, from casual puzzles to graphically stunning adventures. The convenience and versatility of mobile gaming have expanded the gaming community, allowing anyone to dive into games anytime, anywhere.',
+            Label: 'Mobile Gaming',
+            Date: '2023-11-07',
+        },
+    ],
 }

e39016aa8dda7612807c683af0b7bef2a617ddad
saba2446 <sabakhara44@gmail.com>
2023-11-17 14:41:40 +0400

    FIX: Build Error

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { minify } from 'terser'
+// import { minify } from 'terser'
+import path from 'path'
 export default defineConfig({
     plugins: [react()],
 
@@ -10,20 +11,25 @@ export default defineConfig({
     },
 
     build: {
-        outDir: 'dist',
-        minify: 'terser',
-        sourcemap: true,
-        cssCodeSplit: true,
-        terserOptions: {
-            compress: {
-                drop_console: true,
-            },
-            minify,
-        },
+        modulePreload: false,
+        target: 'esnext',
+        minify: false,
+        cssCodeSplit: false,
+        // outDir: 'dist',
+        // minify: 'terser',
+        // sourcemap: true,
+        // cssCodeSplit: true,
+        // terserOptions: {
+        //     compress: {
+        //         drop_console: true,
+        //     },
+        //     // minify,
+        // },
     },
     resolve: {
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
         alias: {
-            '@': '/src',
+            '@': path.resolve(__dirname, './src'),
         },
     },
 

59e41f4874ace81dc5c54a7f02827075218803eb
saba2446 <sabakhara44@gmail.com>
2023-11-17 17:52:16 +0400

    #NOTE - Unfinished

--- src/pages/contact/ContactUs.jsx
+++ src/pages/contact/ContactUs.jsx
@@ -1,5 +1,23 @@
-const ContactUs = () => {
+import ImageMap from '@/assets/svgComponents/ImageMap'
+import style from '@/pages/contact/ContactUs.module.scss'
 
+const ContactUs = () => {
+    return (
+        <>
+            <section className={style.contactMap}>
+                <div className={style.commonDetails}>
+                    <h1>Lorem Ipsum is simply dummy text of the printing and.</h1>
+                    <p data-aos="fade-up">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                    <div
+                        className={style.map}
+                        data-aos="zoom-in"
+                    >
+                        <ImageMap />
+                    </div>
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default ContactUs
